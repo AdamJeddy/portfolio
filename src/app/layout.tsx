6044947a961e0b8e5b44d0ed25ccdadd349e30d8
Adam <58102753+AdamJeddy@users.noreply.github.com>
2026-07-10 22:13:32 +0400

    update meta description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const spaceGrotesk = Space_Grotesk({
 
 export const metadata: Metadata = {
   title: "Adam's Portfolio",
-  description: 'Software engineer · AI · Data',
+  description: 'This is literally my portfolio',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
